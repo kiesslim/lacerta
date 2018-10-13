e55d20a6e98c95f5f5bedde1597f9577b65d31a1
mpkagel <mpkagel@gmail.com>
2018-10-12 17:47:30 -0700

    Added front-end container layout, started heading, title, nav buttons.

--- lacerta-app/lacerta-app.py
+++ lacerta-app/lacerta-app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3.6
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    print("hi...")
-    return "Hello World!"
+def hello(name=None):
+	print("hi...")
+	return render_template('layout.html', name=name)
 
 if __name__ == '__main__':
     app.run()
---
+++ lacerta-app/templates/js/container0.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+	$('#nav_link_graph').click( function (e) {
+		$('html, body').animate({
+            scrollTop: $("#container4").offset().top
+        }, 200);
+	});
+});
