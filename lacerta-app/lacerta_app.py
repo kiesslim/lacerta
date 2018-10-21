c298cc7d52ebab994f00fc946e5af19556908231
Meredith Kiessling <kiesslim@oregonstate.edu>
2018-10-21 11:41:30 +0200

    flasks3 debugging & new web-parser.py

--- lacerta-app/lacerta_app.py
+++ lacerta-app/lacerta_app.py
@@ -12,13 +12,6 @@ s3 = FlaskS3(app)
 @app.route("/")
 def hello(name=None):
 	print("hi...")
-	print(s3)
-	print(url_for('static', filename='test.html'))
-	#print(url_for)
-	#print(app.jinja_env.globals)
-	#app.jinja_env.globals.update(url_for=url_for)
-	#print(app.jinja_env.globals)
-	#print(app.jinja_env.globals['url_for']('static', filename='test3.html'))
 	return render_template('layout.html', name=name)
 
 if __name__ == '__main__':
