5164b471c3fdaee80a598c81efffb8ce085b500e
mpkagel <mpkagel@gmail.com>
2018-10-24 02:13:43 -0700

    Added form, started processing form data on backend.

--- lacerta-app/lacerta_app.py
+++ lacerta-app/lacerta_app.py
@@ -2,37 +2,41 @@
 
 from flask import Flask, jsonify, render_template, request
 from flask_s3 import FlaskS3, url_for
-import sys,os
+import sys, os
+from os import path
 sys.path.insert(0, "{}/crawler".format(os.getcwd()))
 import search
 
-
 app = Flask(__name__)
-app.config['FLASKS3_BUCKET_NAME'] = 'lacerta-app'
+app.config['FLASKS3_BUCKET_NAME'] = 'lacerta-app2'
 s3 = FlaskS3(app)
 
 @app.route("/")
 def hello(name=None):
 	return render_template('layout.html', name=name)
 
-@app.route("/bfs", methods=['POST'])
-def bfs():
-	start = request.json.get('start_url')
-	depth = request.json.get('depth')
-	keyword = request.json.get('keyword')
+@app.route("/query", methods=['POST'])
+def query(name=None):
+	start = request.form["start_url"]
+	depth = request.form["depth"]
+	keyword = request.form["keyword"]
+	search_type = request.form["search_type"]
 	if not start or not depth:
-		return jsonify({'Error: start URL and search depth required!'}), 400
-	return search.bfs(start, depth, keyword)
+	 	return jsonify({'Error: start URL and search depth required!'}), 400
+	if search_type == "BFS":
+		result = search.bfs(start, depth, keyword)
+	elif search_type == "DFS":
+		result = search.dfs(start, depth, keyword)
+	return render_template('layout.html', name=name, result=result)
 
-@app.route("/dfs", methods=['POST'])
-def dfs():
-	start = request.json.get('start_url')
-	depth = request.json.get('depth')
-	keyword = request.json.get('keyword')
-	if not start or not depth:
-		return jsonify({'Error: start URL and search depth required!'}), 400
-	return search.dfs(start, depth, keyword)
-
+# @app.route("/dfs", methods=['POST'])
+# def dfs():
+# 	start = request.json.get('start_url')
+# 	depth = request.json.get('depth')
+# 	keyword = request.json.get('keyword')
+# 	if not start or not depth:
+# 		return jsonify({'Error: start URL and search depth required!'}), 400
+# 	return search.dfs(start, depth, keyword)
 
 if __name__ == '__main__':
     app.run()
---
+++ lacerta-app/templates/js/container3.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+	$('#new_query_button').click( function (e) {
+		var package = {};
+		package["start_url"] = $("#start_url").val();
+		package["depth"] = $("#depth").val();
+		package["keyword"] = $("#keyword").val();
+		package["search_type"] = $("input[name='search_type']:checked").val();
+        $.ajax({
+            type: "POST",
+            url: "/query",
+            data: package,
+            success: function(data) {
+                window.location.href = "/";
+            }
+        });
+        event.preventDefault();
+	});
+});
