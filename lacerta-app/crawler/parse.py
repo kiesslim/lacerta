bf805241e06d0319221958c0949d050cc9d8ebdc
Meredith Kiessling <kiesslim@oregonstate.edu>
2018-10-25 15:19:46 +0200

    working search algorithms

--- lacerta-app/crawler/parse.py
+++ lacerta-app/crawler/parse.py
@@ -12,14 +12,22 @@ from urllib.parse import urldefrag, urlparse, urlsplit, urljoin
 class Web:
     def __init__(self, url):
         # TODO: add error handling for URL
-        self.validate_url(url)
+        print(url)
+        if not validate_url(url):
+            raise ValueError('Invalid  URL: URL {} is invalid'.format(url))
+            return None
+        r = get_response(url)
+        if r.status_code is not 200:
+            return None
         self.url = self.normalize(url)
-        self.response = self.get_response()
-        self.html = self.get_html()
+        self.response = r
+        self.status_code = self.response.status_code
         self.urls = set()
+        self.html = self.get_html()
         self.get_urls_from_html()
         self.title = self.get_title_from_html()
 
+    # TODO: remove query strings!!
     def normalize(self, url):
         result = urlsplit(url)
         return urldefrag(result.geturl())[0]
@@ -31,28 +39,39 @@ class Web:
         return urljoin(self.url, url)
 
     def get_html(self):
-        return html.fromstring(self.response.content)
-
-    def get_response(self):
-        return requests.get(self.url)
+        if self.status_code is 200 and self.response.content:
+            return html.fromstring(self.response.content)
+        return None
 
     ''' remove styling/javascript/scripts before parsing '''
     #TODO: fix bug www.mysite.org AND www.mysite.org/ both return
     def get_urls_from_html(self):
+        if self.html is None:
+            return
         cleaner = Cleaner()
         cleaner.javascript = True
         cleaner.scripts = True
         cleaner.style = True
         cleaned = html.fromstring(cleaner.clean_html(self.response.content))
         raw_urls = cleaned.xpath('//a/@href')
+        #TODO: validate_urls before adding?
         return [self.urls.add(self.absolute_url(self.normalize(url))) for url in raw_urls]
 
     def get_title_from_html(self):
+        if self.html is None:
+            return None
         return self.html.findtext('.//title')
 
-    #TODO:add error handling. raise ValueError or exception?
-    # note: this may not be needed. Invalid urls could just be represented as
-    # dead-end nodes
-    def validate_url(self, url):
-        components = urlparse(url)
-        return components.scheme and components.netloc
+    # TODO: 
+    def get_text(self):
+        return
+
+#TODO:add error handling. raise ValueError or exception?
+# note: this may not be needed. Invalid urls could just be represented as
+# dead-end nodes
+def validate_url(url):
+    components = urlparse(url)
+    return components.scheme and components.netloc
+
+def get_response(url):
+    return requests.get(url)
--- lacerta-app/crawler/search.py
+++ lacerta-app/crawler/search.py
@@ -1,17 +1,45 @@
 #!/usr/bin/python3.6
 
 from flask import jsonify
-import parse
 import json
+from parse import Web, validate_url
+from queue import Queue
 
+
+MAX_NODES=100
 
 class Node:
-    def __init__(self, url, title):
-        self.url = url
-        self.edges = []
-        self.title = title
+    def __init__(self, web):
+        if web is None:
+            return None
+        if not validate_url(web.url) or web.status_code is not 200:
+            return None
+        self.url = web.url
+        self.edges = web.urls
+        self.title = web.title
         self.has_keyword = False
 
+    def __str__(self):
+        if self is None:
+            return
+        str = 'url: {}, title: {}, has_keyword: {}, edges:'.format(self.url, self.title, self.has_keyword)
+        for edge in self.edges:
+            str.join(' {},'.format(edge))
+        return str
+
+# graph = {
+#     'start_url' : start,
+#     'search_type' : 'bfs',
+#     'keyword' : keyword,
+#     'nodes' : {
+#         'url1': {
+#             'url' : 'url1',
+#             'title' : 'TITLE',
+#             'has_keyword' : False ,
+#             'edges' : ['url2','url3']
+#         }
+#     }
+# }
 class Graph:
     def __init__(self, start, depth, keyword, search_type):
         self.start_url = start
@@ -20,50 +48,62 @@ class Graph:
         self.search_type = search_type
         self.nodes = dict()
 
-    def add_node(node):
-        if node.url in self.nodes:
-            #TODO: error handling
-            return
+    def add_node(self, node):
         self.nodes[node.url]=node
 
-#toVisit = Queue
-def bfs(start, depth, keyword):
-    graph = {
-        'start_url' : start,
-        'search_type' : 'bfs',
-        'keyword' : keyword,
-        'nodes' : {
-            'url1': {
-                'url' : 'url1',
-                'title' : 'TITLE',
-                'has_keyword' : False ,
-                'edges' : ['url2','url3']
-            }
-        }
-    }
-    # start = Web(start_url)
-    # toVisit= Queue()
-    # visited = set()
-    # toVisit.put(start)
-    #
-    # while not toVisit.is_empty():
-    #     current = toVisit.get()
-        #add all neighbors to toVisit if it doesnt contain it
-    return json.dumps(graph, indent=True)
+'''search validates form data, and then calls appropriate search function'''
+def search(start_url, max_depth, keyword, search_type):
+    g = Graph(start_url, max_depth, keyword, search_type)
+    if not validate_url(start_url):
+        raise ValueError('Invalid Start URL: {}'.format(start_url))
+    if search_type is 'BFS':
+        return bfs(g, 0, max_depth)
+    elif search_type is 'DFS':
+        return dfs(g, 0, max_depth)
+    else:
+        raise ValueError('Invalid Search Type: Specify BFS or DFS')
 
-#toVisit = stack
-def dfs(start, depth, keyword):
-    graph = {
-        'start_url' : start,
-        'search_type' : 'bfs',
-        'keyword' : keyword,
-        'nodes' : {
-            'url1': {
-                'url' : 'url1',
-                'title' : 'TITLE',
-                'has_keyword' : False ,
-                'edges' : ['url2','url3']
-            }
-        }
-    }
-    return json.dumps(graph, indent=True)
+'''Note: bfs/dfs look the same, but will be different when depth handled'''
+#TODO: implement depth and keyword
+def bfs(graph, current_depth, max_depth):
+    start = graph.start_url
+    toVisit= Queue()
+    toVisit.put(start)
+
+    # TODO: remove len(g) <100 and replace with current_depth < max_depth
+    while not toVisit.empty() and len(graph.nodes) < MAX_NODES:
+        current = toVisit.get()
+        node = Node(Web(current))
+        if node is not None:
+            graph.add_node(node)
+            for edge in node.edges:
+                if edge not in graph.nodes:
+                    toVisit.put(edge)
+    print('\n\n\n\n\nPrinting Graph:\n\n\n\n\n')
+    for k,v in graph.nodes.items():
+        print('{}: {}\n'.format(k, v))
+    return graph
+
+'''Note: bfs/dfs look the same, but will be different when depth handled'''
+#TODO: implement depth and keyword
+def dfs(graph, current_depth, max_depth):
+    start = graph.start_url
+    #toVisit is a stack containing next nodes
+    toVisit= []
+    toVisit.append(start)
+    print(toVisit)
+
+    # TODO: remove len(g) <100 and replace with current_depth < max_depth
+    while toVisit and len(graph.nodes) < MAX_NODES:
+        current = toVisit.pop()
+        node = Node(Web(current))
+        if node is not None:
+            graph.add_node(node)
+            for edge in node.edges:
+                if edge not in graph.nodes:
+                    toVisit.append(edge)
+
+    print('\n\n\n\n\nPrinting Graph:\n\n\n\n\n\n')
+    for k,v in graph.nodes.items():
+        print('{}: {}\n'.format(k, v))
+    return graph
