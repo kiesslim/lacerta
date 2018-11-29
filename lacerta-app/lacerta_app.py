a26309542bd2bfeef9b587b40331e18952502b64
Meredith Kiessling <kiesslim@oregonstate.edu>
2018-11-29 13:18:41 +0100

    fixed json response bug

--- lacerta-app/lacerta_app.py
+++ lacerta-app/lacerta_app.py
@@ -81,7 +81,7 @@ def query():
 			if not result.nodes:
 				return bad_request('Oops! Empty Graph! Try another start URL')
 			else:
-				response = make_response(jsonify(result_json_d3), 200)
+				response = make_response(result_json_d3, 200)
 				response.set_cookie('history', history)
 				return response
 		except ValueError as error:
